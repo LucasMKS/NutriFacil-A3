6b0c945b48a8aefc6ce8f3247a31ab0e36677356
LucasMKS <lucas.mar17ques@gmail.com>
2025-05-23 09:45:46 -0300

    first commit

---
+++ src/main/java/com/gqs/nutrifacil/NutrifacilApplication.java
@@ -0,0 +1,13 @@
+package com.gqs.nutrifacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrifacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrifacilApplication.class, args);
+	}
+
+}
