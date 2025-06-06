6258bb636af6c4f737c95d1975df70eaae117e79
Gabriel <90053550+GabrielSGaldino@users.noreply.github.com>
2025-06-06 13:58:59 -0300

    TmbServiceTest.java
    fix: correção no valor da variavel do teste

--- src/test/java/com/gqs/nutrifacil/service/TmbServiceTest.java
+++ src/test/java/com/gqs/nutrifacil/service/TmbServiceTest.java
@@ -14,7 +14,7 @@ class TmbServiceTest {
         CalculoTmbRequest req = new CalculoTmbRequest();
         req.setSexo("M");
         req.setPeso(70);
-        req.setAltura(175);
+        req.setAltura(1.75);
         req.setIdade(25);
         CalculoTmbResponse resp = service.calcularTmb(req);
         // O valor exato depende da fórmula (usando Harris-Benedict)
@@ -26,7 +26,7 @@ class TmbServiceTest {
         CalculoTmbRequest req = new CalculoTmbRequest();
         req.setSexo("F");
         req.setPeso(60);
-        req.setAltura(165);
+        req.setAltura(1.65);
         req.setIdade(30);
         CalculoTmbResponse resp = service.calcularTmb(req);
         assertEquals(1320.25, Math.round(resp.getTmb() * 100.0) / 100.0);
